73cc95082eda30b38de8a194d6185c3f676bf37e
Dev Patel <49341610+Virus2466@users.noreply.github.com>
2025-08-13 01:04:47 +0530

    Texture Loading

--- Texture.cpp
+++ Texture.cpp
@@ -1,3 +1,18 @@
 #include "Texture.h"
+#include "WICTextureLoader.h"
+#pragma comment(lib, "DirectXTK.lib")
 
+ID3D11ShaderResourceView* LoadTextureFromFile(ID3D11Device* device, const wchar_t* filename)
+{
+	ID3D11ShaderResourceView* texture = nullptr;
+	DirectX::CreateWICTextureFromFile(device, filename, nullptr, &texture);
+	return texture;
+}
+
+void ReleaseTexture(ID3D11ShaderResourceView* texture)
+{
+	if (texture) {
+		texture->Release();
+	}
+}
 
